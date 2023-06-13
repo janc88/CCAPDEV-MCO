dc06ef03090ee02de84b098030ef7e368c7dc959
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-06-13 21:31:10 +0800

    Add files via upload

---
+++ Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import {
+  Logo,
+  LogoLink,
+  Nav,
+  NavSection,
+  NavMenu,
+  NavbarLink,
+} from "./styles/Nav.styled";
+import { LogoContainer } from "./styles/NavSearch.styled";
+import { Button } from "../../styles/Button.styled";
+import logo from "../../imgs/banana.svg";
+import UserCard from "./UserCard";
+import NavSearch from "./NavSearch";
+
+function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <Nav>
+	  <LogoContainer>
+		<LogoLink to="/home">
+			<Logo src={logo} />
+		</LogoLink>
+		<NavSearch/>
+	  </LogoContainer>
+
+      <NavMenu>
+        <NavbarLink to="/">Home</NavbarLink>
+        <NavbarLink to="/restaurants">Restaurants</NavbarLink>
+        <NavbarLink to="/">About Us</NavbarLink>
+        <NavbarLink to="/">Contact Us</NavbarLink>
+      </NavMenu>
+      <NavSection>
+        {!isLoggedIn ? (
+          <>
+            <NavbarLink to="/">Log In</NavbarLink>
+            <Button bgcolor="white" tcolor="black">
+              Sign up
+            </Button>
+          </>
+        ) : (
+          <UserCard />
+        )}
+      </NavSection>
+    </Nav>
+  );
+}
+
+export default Navbar;
