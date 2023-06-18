d4e0f6c4b357d9f397f4d6ed91cf6058eca67860
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-06-18 10:06:31 +0800

    Add flex utils

---
+++ src/styles/Flex.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const FlexRow = styled.label`
+	display: flex;
+	align-items: center;
+	gap: 5px;
+`;
+
+export const FlexCol = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
+`;
+
+export const FlexRight = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	gap: 5px;
+`
