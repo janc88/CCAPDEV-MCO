106a1b836652e229c5aac330d1f1b774095eabe2
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-06-23 12:24:30 +0800

    Update SearchResultsBar.tsx

--- src/pages/SearchPage/SearchResultsBar.tsx
+++ src/pages/SearchPage/SearchResultsBar.tsx
@@ -1,10 +1,19 @@
-import { SearchContainer, SearchIcon } from "./SearchBarResults.styled";
+import { useState } from "react";
+import { SearchContainer, SearchIcon, SearchInput } from "./SearchBarResults.styled";
 
 const SearchResultsBar = () => {
+	const [isInputFocused, setIsInputFocused] = useState(false);
+	const handleInputFocus = () => setIsInputFocused(true);
+	const handleInputBlur = () => setIsInputFocused(false);
+
 	return (
 		<SearchContainer>
-            <SearchIcon/>
-			Search results for "text"
+            {isInputFocused || <SearchIcon/>}
+			<SearchInput 
+				onFocus={handleInputFocus} 
+				onBlur={handleInputBlur} 
+				hasIcon={!isInputFocused}
+				placeholder="Search results for 'text'"/>
 		</SearchContainer>
 	);
 };
