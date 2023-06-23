88d5a979d3e494db65fbf5d4ccca4f549f0e05eb
nidorb <gregory_charles_tiong@dlsu.edu.ph>
2023-06-23 15:37:14 +0800

    updated write so need to sign up first

--- src/components/ReviewsCard/ReviewsCard.tsx
+++ src/components/ReviewsCard/ReviewsCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   Header,
   RestoReviewsContainer,
@@ -7,10 +7,12 @@ import {
 } from "./ReviewsCard.styled";
 import ReviewCard from "../ReviewCard/ReviewCard";
 import ViewWriteModal from "../ModalPopups/ViewWriteModal";
+import SmallModal from "../SmallModal/SmallModal";
+import { UserContext } from "../../contexts/UserContext";
 
 interface ReviewsCardProps {
   reviewList: ReviewProps[];
-  showOverLay?: boolean; //show overlay of restoname in the review image
+  showOverLay?: boolean; // show overlay of restoname in the review image
 }
 
 export interface ImageProps {
@@ -38,27 +40,49 @@ const ReviewsCard: React.FC<ReviewsCardProps> = ({
   showOverLay = false,
 }) => {
   const [showWriteModal, setshowWriteModal] = useState(false);
+  const [isSmallModalVisible, setIsSmallModalVisible] = useState(false);
+
+  const { user } = useContext(UserContext);
 
   const toggleWriteModal = () => {
     setshowWriteModal((prevshowWriteModal) => !prevshowWriteModal);
   };
-  
+
+  const toggleSmallModal = () => {
+    setIsSmallModalVisible((wasModalVisible) => !wasModalVisible);
+  };
+
   return (
     <RestoReviewsContainer isUserReview={showOverLay}>
       <Header>{showOverLay ? "User's Reviews" : "Restaurant's Reviews"}</Header>
       <ReviewsContainer isUserReview={showOverLay}>
         {reviewList.map((review) => (
-          <ReviewCard {...review} showOverlay={showOverLay} />
+          <ReviewCard key={review.id} {...review} showOverlay={showOverLay} />
         ))}
       </ReviewsContainer>
 
-      {!showOverLay && <WriteReview onClick={toggleWriteModal}/>}
+      {!showOverLay && (
+        <WriteReview
+          onClick={() => {
+            if (user) {
+              toggleWriteModal();
+            } else {
+              toggleSmallModal();
+            }
+          }}
+        />
+      )}
 
       <ViewWriteModal
-              isModalVisible={showWriteModal}
-              onBackdropClick={toggleWriteModal}
-              {...reviewList[1]} //temporary only
-        />
+        isModalVisible={showWriteModal}
+        onBackdropClick={toggleWriteModal}
+        {...reviewList[1]} // temporary only
+      />
+
+      <SmallModal
+        isModalVisible={isSmallModalVisible}
+        onBackdropClick={toggleSmallModal}
+      />
     </RestoReviewsContainer>
   );
 };
