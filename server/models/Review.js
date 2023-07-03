7031af4feee7c54679cebfb52634244a4db0a56f
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-07-03 16:14:25 +0800

    changed schema

--- server/models/Review.js
+++ server/models/Review.js
@@ -4,6 +4,16 @@ const ReviewSchema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
   datePosted: { type: Date, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  restaurant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
   stars: { type: Number, required: true },
   helpful: { type: Number, required: true },
   ownerResponse: { type: Number, required: true },
--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    description: {type: String, required: true},
-    password: {type: String, required: true},
-    avatar: {type: String, required: true},
-})
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: { type: String, required: true },
+  },
+  {
+    _id: false,
+  }
+);
 
-const userModel = mongoose.model('User', UserSchema);
+const userModel = mongoose.model("User", UserSchema);
 
 export default userModel;
