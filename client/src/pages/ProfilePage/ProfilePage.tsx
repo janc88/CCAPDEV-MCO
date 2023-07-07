9f3abb0806281d8dd25177f0d3e165cd8a9a946d
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-07-07 19:54:25 +0800

    Update edit profile page

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -7,27 +7,20 @@ import {
 } from "./ProfilePage.styled";
 import ReviewsCard from "../../components/ReviewsCard/ReviewsCard";
 import {
-  ImageProps,
   ReviewProps,
 } from "../../components/ReviewsCard/ReviewsCard";
 import UserInfoCard from "../../components/UserInfoCard/UserInfoCard";
 
 interface ProfilePageProps {
-  username: string;
-  description: string;
-  profilePic: ImageProps;
   reviews: ReviewProps[];
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = (userInfo) => {
+  
   return (
     <ProfilePageContainer>
       <LeftContainer>
-        <UserInfoCard
-          username={userInfo.username}
-          description={userInfo.description}
-          profilePic={userInfo.profilePic}
-        />
+        <UserInfoCard/>
       </LeftContainer>
       <RightContainer>
         <ReviewsCard reviewList={userInfo.reviews} showOverLay />
