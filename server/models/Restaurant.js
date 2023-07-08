a84b7b68d08ae086d1e5a525322487e5728ab481
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-07-08 20:50:19 +0800

    change resto schema

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -6,6 +6,16 @@ const RestaurantSchema = new mongoose.Schema({
   address: { type: String, required: true },
   coverImg: { type: String, required: true },
   imgs: [{ type: String, required: true }],
+  starCount: {
+    type: [Number],
+    required: true,
+    default: [0, 0, 0, 0, 0],
+    validate: {
+      validator: function (v) {
+        return v.length === 5;
+      },
+    },
+  },
 });
 
 const restaurantModel = mongoose.model("Restaurant", RestaurantSchema);
