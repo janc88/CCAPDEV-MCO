ea49984978e2493f07e8f4df04bd65f7a2412433
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-07-15 23:18:00 +0800

    Update restaurant controller

--- server/controllers/restaurant.controller.js
+++ server/controllers/restaurant.controller.js
@@ -1,11 +1,78 @@
 import Restaurant from "../models/Restaurant.js";
 import User from "../models/User.js";
+import { uploadImage } from "./image.controller.js";
 
-const getAllRestaurants = async (req, res) => {};
-const getRestaurantDetails = async (req, res) => {};
-const createRestaurant = async (req, res) => {};
-const updateRestaurant = async (req, res) => {};
-const deleteRestaurant = async (req, res) => {};
+const getAllRestaurants = async (req, res) => {
+  res.status(501).send('Not Implemented');
+};
+const getFeaturedRestaurants = async (req, res) => {
+	try {
+		const restaurants = await Restaurant.find({}).limit(10);
+		res.status(200).json(restaurants);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+}
+const getRestaurantDetails = async (req, res) => {
+  try {
+	const { id } = req.params;
+
+	const restaurant = await Restaurant.findById(id)
+		.populate('allReviews');
+
+	if (!restaurant) {
+		return res.status(404).json({ error: "Restaurant not found" });
+	}
+
+	res.status(200).json(restaurant);
+  } catch (error) {
+	res.status(500).json({ error: error.message });
+  }
+};
+const createRestaurant = async (req, res) => {
+  try {
+	const { 
+		name, 
+		description, 
+		address,
+	} = req.body;
+	const coverImg = req.file;
+	const imgs = req.files;
+
+	if (await Restaurant.findOne({ name })) {
+		return res.status(409).json({ error: "Restaurant already exists" });
+	}
+
+	const session = await mongoose.startSession();
+	session.startTransaction();
+
+	const newCoverImg = await uploadImage(coverImg, session);
+	const newImgs = await Promise.all(
+		imgs.map(img => uploadImage(img, session))
+	);
+
+	const newRestaurant = new Restaurant({
+		name,
+		description,
+		address,
+		coverImg: newCoverImg._id,
+		imgs: newImgs.map(img => img._id),
+	});
+	await newRestaurant.save({session});
+	await session.commitTransaction();
+	session.endSession();
+
+	res.status(201).json(newRestaurant);
+  } catch (error) {
+	res.status(500).json({ error: error.message });
+  }
+};
+const updateRestaurant = async (req, res) => {
+  res.status(501).send('Not Implemented');
+};
+const deleteRestaurant = async (req, res) => {
+  res.status(501).send('Not Implemented');
+};
 
 export {
   getAllRestaurants,
@@ -13,4 +80,5 @@ export {
   createRestaurant,
   updateRestaurant,
   deleteRestaurant,
+  getFeaturedRestaurants,
 };
--- server/routes/restaurant.routes.js
+++ server/routes/restaurant.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 
 import {
   getAllRestaurants,
@@ -6,13 +7,16 @@ import {
   createRestaurant,
   updateRestaurant,
   deleteRestaurant,
+  getFeaturedRestaurants,
 } from "../controllers/restaurant.controller.js";
 
 const router = express.Router();
+const upload = multer();
 
 router.route("/").get(getAllRestaurants);
+router.route('/featured').get(getFeaturedRestaurants)
 router.route("/:id").post(getRestaurantDetails);
-router.route("/").post(createRestaurant);
+router.route("/").post(upload.single('coverImg'), upload.array('imgs'), createRestaurant);
 router.route("/:id").patch(updateRestaurant);
 router.route("/:id").get(deleteRestaurant);
 
