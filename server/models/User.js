3a32039863edc1b34d96d0edc0c2dc54f7c99b8d
qwerttyuiiop <sherwynnangeles@192.168.68.101>
2023-08-01 20:37:12 +0800

    change back to original schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { 
-	type: String, 
-	required: true, 
-	unique: true,
-  },
+  username: { type: String, required: true, unique: true },
   description: { type: String },
   password: { type: String, required: true },
   avatar: {
