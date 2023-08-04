38fbdaa87b18ce7cf6bc5164b70fc6cbf68a81fe
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-08-04 23:08:17 +0800

    create min-test

--- server/min-test.js
+++ server/min-test.js
@@ -5,12 +5,37 @@ import mongoose from "mongoose";
 import session from "express-session";
 import MongoDBStore from "connect-mongodb-session"
 
-import userRouter from "./routes/user.routes.js";
-import reviewRouter from "./routes/review.routes.js";
-import restaurantRouter from "./routes/restaurant.routes.js";
-import imageRouter from "./routes/image.routes.js";
-import ownerRouter from "./routes/owner.routes.js";
+const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const hashedPassword = crypto.SHA256(password).toString();
 
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		} else if (user.password !== hashedPassword) {
+			return res.status(401).json({ error: "Wrong password" });
+		}
+
+		req.session.userId = user._id.toString();
+		res.status(200).json(user.userInfo());
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+const getLoggedInUser = async (req, res) => {
+	try {
+	  const { userId } = req.session;
+	  const user = await User.findById(userId);
+	  if (user)
+		res.status(200).json(user.userInfo());
+	  else
+		res.status(404).json({ message: 'User not found' });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+};
 
 dotenv.config();
 
@@ -42,15 +67,12 @@ app.use(
 
 app.get("/", async (req, res) => {
 	res.send({ 
-		message: "Hello World",
+		message: "backend test 8",
 	});
 });
 
-app.use("/api/users", userRouter);
-app.use("/api/reviews", reviewRouter);
-app.use("/api/restaurants", restaurantRouter);
-app.use("/api/images", imageRouter);
-app.use("/api/owners", ownerRouter);
+app.post('api/users/login', loginUser);
+app.get('api/users/me', getLoggedInUser);
 
 const connectDB = (url) => {
 	mongoose.set("strictQuery", true);
