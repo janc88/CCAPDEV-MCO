f81aa135f1ce5cfa3f702b59dff3c07e6833a696
qwerttyuiiop <sherwynnangeles@192.168.68.101>
2023-08-05 15:10:17 +0800

    revert test chanegs

--- server/min-test.js
+++ server/min-test.js
@@ -4,22 +4,74 @@ import cors from "cors";
 import mongoose from "mongoose";
 import session from "express-session";
 import MongoDBStore from "connect-mongodb-session"
+import User from "./models/User.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
-import userRouter from "./routes/user.routes.js";
-import reviewRouter from "./routes/review.routes.js";
-import restaurantRouter from "./routes/restaurant.routes.js";
-import imageRouter from "./routes/image.routes.js";
-import ownerRouter from "./routes/owner.routes.js";
+const loginUser = async (req, res) => {
+	try {
+		const user = await User.findOne({ username: "12345678" });
 
+		req.session.regenerate(async (err) => {
+			if (err) {
+				res.status(500).json({ error: err.message });
+			} else {
+				req.session.userId = user._id.toString();
+				res.status(200).json({
+					user: user.userInfo(),
+					sessionID: req.sessionID,
+					session: req.session,
+				});
+			}
+		});
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+const getLoggedInUser = async (req, res) => {
+	try {
+	  const { userId } = req.session;
+	  const user = await User.findById(userId);
+	  if (user)
+		res.status(200).json({
+			user: user.userInfo(),
+			sessionID: req.sessionID,
+			session: req.session,
+		});
+	  else
+		res.status(404).json({ 
+			message: 'User not found',
+			sessionID: req.sessionID,
+			session: req.session,
+		});
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+};
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser(process.env.SESSION_SECRET));
+
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+// 	res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-   Type, Accept, Authorization");
+// 	next();
+// });
+// app.set('trust proxy', 1);
+
 app.use(cors({
 	origin: true,
 	credentials: true,
 }));
-app.use(express.json({ limit: "50mb" }));
 
 const store = new (MongoDBStore(session))({
 	uri: process.env.MONGODB_URL,
@@ -30,6 +82,7 @@ const store = new (MongoDBStore(session))({
 		useUnifiedTopology: true,
 	}
 });
+
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
@@ -38,7 +91,7 @@ app.use(
 		cookie: {
 			maxAge: 30 * 24 * 60 * 60 * 1000,
 			sameSite: "none",
-			secure: true,
+			secure: false,
 		},
 		store: store,
 	})
@@ -46,15 +99,12 @@ app.use(
 
 app.get("/", async (req, res) => {
 	res.send({ 
-		message: "Hello World!",
+		message: "backend test 15",
 	});
 });
 
-app.use("/api/users", userRouter);
-app.use("/api/reviews", reviewRouter);
-app.use("/api/restaurants", restaurantRouter);
-app.use("/api/images", imageRouter);
-app.use("/api/owners", ownerRouter);
+app.post('/login', loginUser);
+app.get('/me', getLoggedInUser);
 
 const connectDB = (url) => {
 	mongoose.set("strictQuery", true);
@@ -70,7 +120,9 @@ const connectDB = (url) => {
 const startServer = async () => {
 	try {
 		connectDB(process.env.MONGODB_URL);
-		app.listen(process.env.PORT, () => console.log("Server started on port", + process.env.PORT));
+		app.listen(process.env.PORT, () => {
+			console.log(`HTTP server is running on port ${process.env.PORT}`);
+		});
 	} catch (error) {
 		console.log(error);
 	}
